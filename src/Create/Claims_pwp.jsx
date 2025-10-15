b21b060846278ca99a8b250ce7cdd6507b8364ce
Mark bryan <markbryan1515@gmail.com>
2025-10-15 10:41:16 +0800

    Update Claims_pwp.jsx

--- src/Create/Claims_pwp.jsx
+++ src/Create/Claims_pwp.jsx
@@ -2318,8 +2318,10 @@ Description: ${selectedDistributor.description?.trim() || "N/A"}`);
                                                                             <label
                                                                                 className="form-check-label"
                                                                                 htmlFor={`cat-check-${cat.id}`}
+                                                                                style={{ marginLeft: '5px' }}
+
                                                                             >
-                                                                                <strong>{cat.code}</strong> - {cat.name}
+                                                                                {cat.name} <strong style={{ color: '#fff' }}>{cat.code}</strong>
                                                                             </label>
                                                                         </div>
                                                                     </li>
@@ -2552,7 +2554,7 @@ Description: ${selectedDistributor.description?.trim() || "N/A"}`);
                                                                     }
                                                                 }}
                                                             >
-                                                                <span>({opt.code}) - {opt.name}</span>
+                                                                <span> {opt.name}</span> <strong style={{ color: '#ffff' }}>({opt.code}) </strong>
                                                                 <FiChevronRight style={{ color: "#888", fontSize: "16px" }} />
                                                             </div>
                                                         ))}
@@ -2636,7 +2638,7 @@ Description: ${selectedDistributor.description?.trim() || "N/A"}`);
                                                                     style={{ marginLeft: "6px", cursor: "pointer" }}
                                                                 >
                                                                     {s.name}{" "}
-                                                                    <span style={{ color: "#888", fontSize: "12px" }}>
+                                                                    <span style={{ color: "#ffffffff", fontSize: "12px" }}>
                                                                         ({s.code})
                                                                     </span>
                                                                 </label>
