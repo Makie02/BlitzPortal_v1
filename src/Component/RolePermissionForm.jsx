c7b2ce1baab57f4dd0c7b3be94d0882f6a2ba22f
Mark bryan <markbryan1515@gmail.com>
2025-10-30 09:38:40 +0800

    Update RolePermissionForm.jsx

--- src/Component/RolePermissionForm.jsx
+++ src/Component/RolePermissionForm.jsx
@@ -9,7 +9,7 @@ const roleCategories = [
   "BrandSelector", "BrandApprovalForm", "Activities", "RecordsPage",
   "ApprovalList", "ManageMarketing", "Calendar", "LoginPage", "AnnouncementForm",
   "RentalsForm", "AddendumCancellation", "ClaimsStatusUpload", "Claims_pwp", "ClaimsRecords",
-  "BudgetDashboard" ,"Analytics"
+  "BudgetDashboard" ,"Analytics","UploadingSap"
 ];
 
 export default function RolePermissionForm({ onSubmit }) {
