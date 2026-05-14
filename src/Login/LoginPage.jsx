f6e679fdc6890e9c9fbb9627ce43edc881be9e4d
Mark bryan <markbryan1515@gmail.com>
2026-05-14 10:51:47 +0800

    Update LoginPage.jsx

--- src/Login/LoginPage.jsx
+++ src/Login/LoginPage.jsx
@@ -290,7 +290,7 @@ function LoginPage({ setLoggedInUser, setCurrentView }) {
                     </form>
 
                     <footer className="login-footer">
-                        <div>Version 2.0.0</div>
+                        <div>Version 2.0.1</div>
                         <div
                             style={{
                                 marginTop: '6px',
